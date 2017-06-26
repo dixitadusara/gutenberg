ea0426ab18b1bdc2f9cdd281d5ea21c7c928af24
Marcus Kazmierczak <marcus@mkaz.com>
2017-06-26 14:27:28 -0700

    Make linter happy

--- blocks/library/gallery/gallery-image.js
+++ blocks/library/gallery/gallery-image.js
@@ -2,7 +2,7 @@
 export default function GalleryImage( props ) {
 	return (
 		<figure className="blocks-gallery-image">
-			<img src={ props.img.url } alt={ props.img.alt } data-postId={ props.img.id }/>
+			<img src={ props.img.url } alt={ props.img.alt } data-postId={ props.img.id } />
 		</figure>
 	);
 }
